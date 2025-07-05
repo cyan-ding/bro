f5e9fa191840bc00c3eb99af3ac08b4a161155db
Cyan Ding <blueplus.d@gmail.com>
2025-07-05 11:11:04 -0700

    fix gitignore, add auto crop script

--- vision/test.py
+++ vision/test.py
@@ -1,4 +1,8 @@
 import torch
 
-print(torch.cuda.is_available())  # Should be True
-print(torch.cuda.get_device_name(0))  # Should print your GPU name
+# print(torch.cuda.is_available())  # Should be True
+# print(torch.cuda.get_device_name(0))  # Should print your GPU name
+
+
+from ultralytics import YOLO
+model = YOLO("runs/detect/train11/weights/best.pt")
